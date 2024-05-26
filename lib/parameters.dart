ba3e91354f8789c1250fe4d42df353ee0abceea0
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-05-26 19:12:49 +0530

    Parameters

--- lib/parameters.dart
+++ lib/parameters.dart
@@ -3,6 +3,7 @@ void main() {
   Map userData = userMap("Atharva", 24, "M", 17);
   print(userData);
   main1();
+  main2();
 }
 
 Map userMap(String name, int age, String gender, int userClass) {
@@ -31,4 +32,19 @@ Map userMap1(
     "ling": gender,
     "userDiv": userClass,
   };
+}
+
+//*Default parameters work only with named parameters, not positional parameters.
+void main2() {
+  // double length = 5.0, breadth = 2.0, radius = 8.0;
+  print(areaRec());
+  print(areaCircle());
+}
+
+double areaRec({double length = 2.0, double breadth = 5.0}) {
+  return length * breadth;
+}
+
+double areaCircle({double radius = 1.0}) {
+  return 3.14 * radius * radius;
 }
