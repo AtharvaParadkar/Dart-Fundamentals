111d0f1246245f6a88cbcae784a1fb47a78d77bd
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-05-26 16:26:00 +0530

    Break Continue

---
+++ lib/breakcontinue.dart
@@ -0,0 +1,16 @@
+//break
+//continue
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 != 0) {
+      print(i * 2);
+    }
+    if(i==5){
+      continue;
+    }
+    if(i==7){
+      break;
+    }
+  }
+  print("Loop is over");
+}
