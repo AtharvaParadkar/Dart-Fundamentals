74c54d2f0dd34168954cb54196b957ae6e320dc8
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-11-12 10:56:46 +0530

    Reverse String in a List

--- lib/practice/rev_list_string.dart
+++ lib/practice/rev_list_string.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 void main(){
   List<String> a=['Abc','Def','Ghi','Jkl','Mno'];
   for(int i=a.length-1;i>=0;i--){
     for(int j=a[i].length-1;j>=0;j--){
-      print(a[i][j]);
+      stdout.write('${a[i][j]} ');
     }
+    print('');
   }
 }
